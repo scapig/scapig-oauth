6c3524937aacba9f9a2304d4e4cf16d910192021
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-01-29 22:04:08 +0000

    update memory and latest version

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sbt universal:package-zip-tarball
 docker build -t scapig-oauth .
-docker tag scapig-oauth scapig/scapig-oauth:0.1
-docker push scapig/scapig-oauth:0.1
+docker tag scapig-oauth scapig/scapig-oauth
+docker push scapig/scapig-oauth
